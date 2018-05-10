447f9318c51b08a849e029217c65d7ce62c2c9dc
Alex Forbes <alex.forbes@starlingbank.com>
2018-05-10 11:02:31 +0100

    Add support for calculating previous tag, improve title

--- cli.js
+++ cli.js
@@ -124,43 +124,108 @@ function generateDiffForPaths(repoPath, baseCommit, headCommit, pathArray, callb
 }
 
 
+function listTags(repoPath, callback, prefix='RELEASE-2.') {
+    git.Repository.open(path.resolve(repoPath, '.git')).then(
+        repo => {
+            return git.Tag.list(repo)
+        })
+        .then(tagList => {
+            let filteredTags = tagList.filter(t => {
+                return t.startsWith(prefix)
+            })
+            return callback(null, filteredTags.sort())
+        })
+        .catch(reason => {
+            return callback(reason, null)
+        });
+}
+
+
+function getPreviousTag(repoPath, tag, callback) {
+    listTags(repoPath, (err, tagList) => {
+        if (err) {
+            return callback(err, null)
+        }
+
+        let i = tagList.indexOf(tag) - 1
+
+        if (i > -1) {
+            return callback(null, tagList[i])
+        } else {
+            return callback("Could not find tag", null)
+        }
+    })
+}
+
+
+function title(name, base, head) {
+    return  `<h1>Diff of "${name}"</h1>` +
+            `<h3>from ${base}</h3>` +
+            `<h3>to ${head}</h3>`
+}
+
+
 function main() {
     program.version('1.0.0')
         .option('-p, --path [path]', 'Path to repository', '.')
         .option('-b, --base <oid>', 'Base tag')
         .option('-h, --head <oid>', 'Head tag')
         .option('-t, --title [title]', 'Title to use for the output html page', null)
+        .option('--tag-prefix [prefix]', 'Tag prefix to filter on', null)
         .option('-c, --component <path>',
             'Optional component to diff. Must be a relative path to a directory within the '
             + 'repository from its root. Other directories at the same level are filtered from the '
             + 'diff. Whole repository is diff\'d if not specified.', null)
-        .parse(process.argv);
 
-    if (typeof program.base === 'undefined') {
-        console.log('base commit must be specified');
+    program.on('--help', () => {
+        console.log('')
+        console.log("If a base tag is not specified, webdiff will list, sort the repository's tags; and use the previous tag to the given one.")
+        console.log("For example, given a repo with tags of ['v3', 'v1', 'v2'], web diff would default to v2 for the base tag when given a head of v3.")
+        console.log("Sorting is simple alphanumeric, so be aware that v1.9 would be treated as newer than than v1.10 as 9 > 1.")
+    })
+
+    program.parse(process.argv);
+
+    if (typeof program.head === 'undefined') {
+        console.log('head tag must be specified');
         process.exit(1);
     }
 
+    let name
+    let diffPaths
     if (program.component) {
-        var name = path.parse(program.component).name
-        var diffPaths = listPathsForDiff(program.path, program.component)
+        name = path.parse(program.component).name
+        diffPaths = listPathsForDiff(program.path, program.component)
         // console.log('Paths:', diffPaths)
     } else {
-        var name = path.parse(program.path).name
-        var diffPaths = ["*"]
+        name = path.parse(program.path).name
+        diffPaths = ["*"]
     }
-    let title = program.title ||
-        `Diff of "${name}"<br />` +
-        `from ${program.base}<br />` +
-        `to ${program.head}`
 
     /*
         Because libgit's pathspec support is limited (does not support exclusions), we chain
         together diffs from a list of paths, rather than generate a single diff from a pathspec.
      */
+    if (program.base) {
+        generateDiffForPaths(program.path, program.base, program.head, diffPaths, (err, diffString) => {
+            console.log(generateHtmlFromDiff(
+                diffString, program.title || title(name, program.base, program.head))
+            )
+        })
+    } else {
+        getPreviousTag(
+            program.path, program.head, (err, baseTag) => {
+                if (err) {
+                    console.log(err)
+                }
+                generateDiffForPaths(program.path, baseTag, program.head, diffPaths, (err, diffString) => {
+                    console.log(generateHtmlFromDiff(
+                        diffString, program.title || title(name, baseTag, program.head))
+                    )
+                })
+            },
+            program.tagprefix
+        )
+    }
 
-    generateDiffForPaths(program.path, program.base, program.head, diffPaths, (err, diffString) => {
-        // console.log(diffString)
-        console.log(generateHtmlFromDiff(diffString, title))
-    })
 }
